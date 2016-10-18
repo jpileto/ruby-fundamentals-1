0ffdcf99f729fc784b0fa2ed44a9e2daf7b83863
jpileto <jp.ileto@icloud.com>
2016-10-17 20:06:39 -0400

    finish exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+tip = 0.2
+puts "I am giving #{55 * tip} for my meal"
+# tip calculation
+
+a = 24
+b = "12"
+puts "#{a.to_s} + #{b}"
+#adding an integer and a string by converting integer into string first
+
+puts "The answer is #{45628 * 7839}."
+#printing output using string interpolation
+
+(10 < 20 && 30 < 20) || !(10 == 11) == false
