6002b31bad8f1913df4c23be48193787971f1487
jpileto <jp.ileto@icloud.com>
2016-10-17 20:35:49 -0400

    done with exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+n = (1..100)
+n.each do |n|
+  if n % 3 == 0 && n % 5 == 0
+    puts "Bitmaker"
+  elsif n % 3 == 0
+    puts "Bit"
+  elsif n % 5 == 0
+    puts "Maker"
+  else
+    puts "#{n}"
+  end
+end
